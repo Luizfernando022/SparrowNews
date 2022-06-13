b65730d53401eddffbeb8b83a160192dabc9cdaf
Luiz Fernando <96695552+Luizfernando022@users.noreply.github.com>
2022-06-13 16:16:35 -0300

    .

--- Scripts/script.js
+++ Scripts/script.js
@@ -2,17 +2,19 @@ let li = document.querySelectorAll('li')[2]
 let body = document.getElementsByTagName('body')[0]
 let menu = document.getElementsByTagName('menu')[0]
 
-body.onclick = function(e){
-    if(e.target != menu){
-     let elementsDropdown = document.querySelectorAll('[data-open=open]')
-        elementsDropdown.forEach(element=>{
-            element.display = "inline-block"
-        })
-        
-    }else{
-        alert('deu ruim')
+body.onclick = function(e){closeDropdownMenu(e,menu)}
+
+
+function closeDropdownMenu(e,menu){
+    if(menu.dataset.open == "open"){
+        if(e.target != menu && e.target != li && e.target.parentElement.parentElement != menu){
+            navDropdown('',menu)
+        }
     }
 }
+
+
+
 li.onclick = function(){navDropdown(this,menu)}
 
 
@@ -20,6 +22,12 @@ function navDropdown(handdleElement,e){
 
     let distTop = handdleElement.offsetHeight + handdleElement.offsetTop
     let distLeft = handdleElement.offsetLeft
+    dropdownVerification(e,distTop,distLeft)
+    
+
+}
+
+function dropdownVerification(e,distTop,distLeft){
 
     if(e.dataset.open == 'close'){
         e.dataset.open = "open"
